16a612e6d5e06b59ad8c1dc654894a8a52185121
John El Basha <johnelbasha@gmail.com>
2020-09-28 14:55:00 +0100

    troubleshoot

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_121816) do
+ActiveRecord::Schema.define(version: 2020_09_28_132443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jets", force: :cascade do |t|
+    t.string "model"
+    t.integer "max_speed"
+    t.integer "max_range"
+    t.integer "price_hourly"
+    t.string "description"
+    t.integer "fuel_consumption"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_jets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_09_28_121816) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jets", "users"
 end
