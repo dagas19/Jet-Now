ca61d4e079a0125ff6ef1161d12c369984a262a6
John El Basha <johnelbasha@gmail.com>
2020-09-30 09:03:37 +0100

    created migration for jets and updated model

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -1,7 +1,7 @@
 class Jet < ApplicationRecord
   belongs_to :user
 
-  validates :user, :model, :max_speed, :max_range, :price_hourly, :description, :fuel_consumption, presence: true
+  validates :user, :model, :max_speed, :max_range, :price_hourly, :description, :fuel_consumption, :passenger_capacity, :location, presence: true
 
   has_one_attached :photo
 
