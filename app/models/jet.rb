3999e835ad4e798f96097b8a0dbcd119b6edc927
Dovydas Savickas <dagas199@gmail.com>
2020-09-30 11:04:05 +0200

    dd

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -5,4 +5,9 @@ class Jet < ApplicationRecord
 
   has_one_attached :photo
 
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
--- db/migrate/20200928143452_create_bookings.rb
+++ db/migrate/20200928143452_create_bookings.rb
@@ -3,7 +3,8 @@ class CreateBookings < ActiveRecord::Migration[6.0]
     create_table :bookings do |t|
       t.integer :total_cost
       t.integer :duration
-      t.string :date
+      t.datetime :start_date
+      t.datetime  :end_date
       t.references :user, null: false, foreign_key: true
       t.references :jet, null: false, foreign_key: true
 
