ca605cf73c2d2e893f00b07f898d1fc5264ea98c
Dovydas Savickas <dagas199@gmail.com>
2020-09-30 16:36:57 +0200

    fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ require 'open-uri'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
 Jet.destroy_all
+User.destroy_all
 
 user1 = User.create(password: "malteser", email: "Diana@gmail.com")
 user2 = User.create(password: "chickens", email: "John@gmail.com")
