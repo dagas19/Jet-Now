b5f0483e52ff438112eb3765006f9973b6975728
Dovydas Savickas <dagas199@gmail.com>
2020-10-01 11:28:17 +0200

    Second user journey backend done

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -1,5 +1,6 @@
 class Jet < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
 
   validates :user, :model, :max_speed, :max_range, :price_hourly, :description, :fuel_consumption, :passenger_capacity, :location, presence: true
 
